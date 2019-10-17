5fc62bd908e6472fa3a019069e2e93889135dbd2
Matthias Berndt <matthias.berndt@cognotekt.com>
2019-10-17 18:02:43 +0200

    generalize from reader to arbitrary applicative

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,9 @@
+import cats.{Applicative, Id}
 import cats.evidence.Is
 import polymorphic.Exists
+import cats.implicits._
+
+import scala.language._
 
 sealed trait Type[A]
 case object DoubleType extends Type[Double]
@@ -10,10 +14,10 @@ case class Var(name: String) extends Expr
 case class DoubleLit(value: Double) extends Expr
 case class Apply(fun: Expr, arg: Expr) extends Expr
 
-case class TypedValue[Ctx, A](value: Ctx => A, tpe: Type[A])
+case class TypedValue[F[_], A](value: F[A], tpe: Type[A])
 
 object Main {
-  type ETypedValue[Ctx] = Exists[TypedValue[Ctx, *]]
+  type ETypedValue[F[_]] = Exists[TypedValue[F, *]]
 
   def typecheck[A, B](typea: Type[A], typeb: Type[B]): Option[Is[A, B]] = {
     (typea, typeb) match {
@@ -33,31 +37,31 @@ object Main {
     }
   }
 
-  def compileFuncall[Ctx, FunType, ArgType](fun: TypedValue[Ctx, FunType], arg: TypedValue[Ctx, ArgType]): Option[ETypedValue[Ctx]] = {
+  def compileFuncall[F[_], FunType, ArgType](fun: TypedValue[F, FunType], arg: TypedValue[F, ArgType])(implicit F: Applicative[F]): Option[ETypedValue[F]] = {
     fun.tpe match {
       case DoubleType => None
       case funtype: FunctionType[funArgType, funRetType] =>
         typecheck(funtype.argType, arg.tpe).map { is =>
-          val funWithAdjustedType = is.substitute[Lambda[X => Ctx => X => funRetType]](fun.value)
-          Exists(TypedValue((ctx: Ctx) => funWithAdjustedType(ctx)(arg.value(ctx)), funtype.retType))
+          val funWithAdjustedType = is.substitute[Lambda[X => F[X => funRetType]]](fun.value)
+          Exists(TypedValue(funWithAdjustedType <*> arg.value, funtype.retType))
         }
     }
   }
 
-  def compile[Ctx](expr: Expr, vars: Map[String, ETypedValue[Ctx]]): Option[ETypedValue[Ctx]] = expr match {
+  def compile[F[_]](expr: Expr, vars: Map[String, ETypedValue[F]])(implicit F: Applicative[F]): Option[ETypedValue[F]] = expr match {
     case Var(name) =>
       vars.get(name)
     case DoubleLit(value) =>
-      Some(Exists(TypedValue(_ => value, DoubleType)))
+      Some(Exists(TypedValue(F.pure(value), DoubleType)))
     case Apply(fun, arg) =>
       for {
-        compiledFun <- compile[Ctx](fun, vars)
-        compiledArg <- compile[Ctx](arg, vars)
+        compiledFun <- compile[F](fun, vars)
+        compiledArg <- compile[F](arg, vars)
         compiledApply <- compileFuncall(Exists.unwrap(compiledFun), Exists.unwrap(compiledArg))
       } yield compiledApply
   }
 
-  def typecheck[Ctx, A](typedValue: ETypedValue[Ctx], atpe: Type[A]): Option[Ctx => A] = {
+  def typecheck[F[_], A](typedValue: ETypedValue[F], atpe: Type[A]): Option[F[A]] = {
     val TypedValue(value, etpe) = Exists.unwrap(typedValue)
     typecheck(etpe, atpe).map(is => is.substitute(value))
   }
@@ -65,15 +69,15 @@ object Main {
   def main(args: Array[String]): Unit = {
     val expr = Apply(Apply(Var("*"), Apply(Apply(Var("+"), DoubleLit(3)), DoubleLit(5))), DoubleLit(7))
     val vars = Map(
-      "+" -> Exists(TypedValue((_: Unit) => (a: Double) => (b: Double) => a + b, FunctionType(DoubleType, FunctionType(DoubleType, DoubleType)))),
-      "*" -> Exists(TypedValue((_: Unit) => (a: Double) => (b: Double) => a * b, FunctionType(DoubleType, FunctionType(DoubleType, DoubleType))))
+      "+" -> Exists(TypedValue[Id, Double => Double => Double](a => b => a + b, FunctionType(DoubleType, FunctionType(DoubleType, DoubleType)))),
+      "*" -> Exists(TypedValue[Id, Double => Double => Double](a => b => a * b, FunctionType(DoubleType, FunctionType(DoubleType, DoubleType))))
     )
 
     (for {
-      compiledExpr <- compile[Unit](expr, vars)
+      compiledExpr <- compile[Id](expr, vars)
       typed <- typecheck(compiledExpr, DoubleType)
     } yield {
-      println(typed(()))
+      println(typed)
     }).getOrElse {
       println("type checking failed")
     }
