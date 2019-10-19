1746d9fe610d86d8b91b9f3ed9249751d1345bf7
Matthias Berndt <matthias.berndt@cognotekt.com>
2019-10-19 21:41:57 +0200

    refactor away cats' Is type

--- src/main/scala/dsllib/Main.scala
+++ src/main/scala/dsllib/Main.scala
@@ -1,6 +1,5 @@
 package dsllib
 
-import cats.evidence.Is
 import cats.implicits._
 import cats.{Applicative, Id}
 import polymorphic.Exists
@@ -23,36 +22,46 @@ case class Apply(fun: Expr, arg: Expr) extends Expr
 
 case class TypedValue[F[_], A](value: F[A], tpe: Type[A])
 
+sealed trait Eq[A, B]
+object Eq {
+  def refl[A]: Eq[A, A] = Refl()
+}
+case class Refl[A]() extends Eq[A, A]
+
 object Main {
 
-  def typecheck[A, B](typea: Type[A], typeb: Type[B]): Option[Is[A, B]] = {
+  def typecheck[A, B](typea: Type[A], typeb: Type[B]): Option[Eq[A, B]] = {
     (typea, typeb) match {
       case (DoubleType, DoubleType) =>
-        Some(Is.refl)
+        Some(Eq.refl[Double])
       case (f1: FunctionType[arga, reta], f2: FunctionType[argb, retb]) =>
         for {
           argaIsArgb <- typecheck(f1.argType, f2.argType)
           retaIsRetb <- typecheck(f1.retType, f2.retType)
         } yield {
-          val x1: Is[arga => reta, arga => reta] = Is.refl[arga => reta]
-          val x2: Is[arga => reta, argb => reta] = argaIsArgb.substitute[Lambda[X => Is[arga => reta, X => reta]]](x1)
-          val x3: Is[arga => reta, argb => retb] = retaIsRetb.substitute[Lambda[X => Is[arga => reta, argb => X]]](x2)
-          x3
+          argaIsArgb match {
+            case _: Refl[x] =>
+              retaIsRetb match {
+                case _: Refl[y] =>
+                  Eq.refl[x => y]
+              }
+          }
         }
       case (_, _) => None
     }
   }
 
+
   def compileFuncall[F[_], FunType, ArgType](fun: TypedValue[F, FunType], arg: TypedValue[F, ArgType])(implicit F: Applicative[F]): Option[ETypedValue[F]] = {
     fun.tpe match {
       case DoubleType => None
       case funtype: FunctionType[funArgType, funRetType] =>
-        typecheck(funtype.argType, arg.tpe).map { is =>
-          val funWithAdjustedType = is.substitute[Lambda[X => F[X => funRetType]]](fun.value)
-          Exists(TypedValue(funWithAdjustedType <*> arg.value, funtype.retType))
+        typecheck(funtype.argType, arg.tpe).map { case _: Refl[x] =>
+          ETypedValue[F](fun.value <*> arg.value)(funtype.retType)
         }
     }
   }
+
 
   def compile[F[_]](expr: Expr, vars: Map[String, ETypedValue[F]])(implicit F: Applicative[F]): Option[ETypedValue[F]] = expr match {
     case Var(name) =>
@@ -67,14 +76,17 @@ object Main {
       } yield compiledApply
   }
 
-  class retrievePartiallyApplied[A] {
-    def apply[F[_]](typedValue: ETypedValue[F])(implicit atpe: Type[A]): Option[F[A]] = {
-      val TypedValue(value, etpe) = Exists.unwrap(typedValue)
-      typecheck(etpe, atpe).map(_.substitute(value))
+  class castPartiallyApplied[A] {
+    def apply[F[_]](typedValue: ETypedValue[F])(implicit targetType: Type[A]): Option[F[A]] = {
+      val TypedValue(value, valueType) = Exists.unwrap(typedValue)
+      def foo[A, B](eq: Eq[A, B], value: F[A]): F[B] = eq match {
+        case _: Refl[A] => value
+      }
+      typecheck[typedValue.T, A](valueType, targetType).map(foo[typedValue.T, A](_, value))
     }
   }
 
-  def retrieve[A]: retrievePartiallyApplied[A] = new retrievePartiallyApplied[A]
+  def cast[A]: castPartiallyApplied[A] = new castPartiallyApplied[A]
 
   def main(args: Array[String]): Unit = {
     val expr = Apply(Apply(Var("*"), Apply(Apply(Var("+"), DoubleLit(3)), DoubleLit(5))), DoubleLit(7))
@@ -85,7 +97,7 @@ object Main {
 
     (for {
       compiledExpr <- compile(expr, vars)
-      typed <- retrieve[Double](compiledExpr)
+      typed <- cast[Double](compiledExpr)
     } yield {
       println(typed)
     }).getOrElse {
